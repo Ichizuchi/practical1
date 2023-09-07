fb5b2bd0efb759cca34da501db165f06b2af5f9e
Ichizuchi <144344807+Ichizuchi@users.noreply.github.com>
2023-09-07 21:15:34 +0300

    Add files via upload

---
+++ App1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+void main()
+{
+	int x, sum = 0, i, kol = 0;
+	printf("Enter numbers\n");
+	for (i = 1; i <= 10; i++)
+	{
+		scanf_s("%d", &x);
+		if ((x < 0)) sum += x;
+		else if (x >= 1.5) kol++;
+	}
+	printf("Summa otricatelnyh chisel=%d\n", sum);
+	printf("Kolichestvo=%d", kol);
+}
